91e7f05d959c1ff70f198d64fe02b0aec2476423
stwhello <sherinpaul2001@gmail.com>
2026-03-13 16:15:26 +0530

    gsap installation and smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger, ScrollSmoother } from "gsap/all";
 import MainLayout from "./layouts/MainLayout";
 
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
 function App() {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1.5,
+      effects: true,
+    });
+  });
+
   return (
-    <MainLayout />
-  )
+    <main className="overflow-x-hidden">
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <MainLayout />
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,21 @@
+import Hero from "../sections/Hero/Hero";
+import About from "../sections/About/About";
+import SelectedWork from "../sections/SelectedWork/SelectedWork";
+import ClientWork from "../sections/ClientWork/ClientWork";
+import WhatIBuild from "../sections/WhatIBuild/WhatIBuild";
+import Contact from "../sections/Contact/Contact";
+
 const MainLayout = () => {
-  return <div className="text-5xl text-red-500">Tailwind is working</div>;
+  return (
+    <>
+      <Hero />
+      <About />
+      <SelectedWork />
+      <ClientWork />
+      <WhatIBuild />
+      <Contact />
+    </>
+  );
 };
 
 export default MainLayout;
